844ba39762502c1972f96984d030abeb931507ba
Tyler Clardy <tclards@gmail.com>
2022-12-24 14:41:16 -0600

    Created Player Health system and new animations for taking damage.

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -8,28 +8,43 @@ public class PlayerLife : MonoBehaviour
     [Header("Tracking References")]
     [SerializeField] private Animator anim;
     [SerializeField] private Rigidbody2D rb;
-
-    private void Start()
-    {
-        
-    }
+    [SerializeField] private PlayerStats ps;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Trap"))
         {
-            Die();
+            TakeDamage();
         }
     }
 
-    private void Die()
-    {
-        rb.bodyType = RigidbodyType2D.Static;   // disable player movement 
-        anim.SetTrigger("Death");               // play death animation
-    }   
-
+    // Helper Functions:  
     private void RestartLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void TakeDamage()
+    {
+        anim.SetTrigger("TakeDamage");
+        ps.TakeDamage(1);
+
+        if (ps.Health == 0)
+        {
+            StartCoroutine(Die());
+        }
+    }
+
+    IEnumerator Die()
+    {
+        rb.bodyType = RigidbodyType2D.Static;   // disable player movement 
+
+        yield return new WaitForSeconds(1f);
+
+        anim.SetTrigger("Death");               // play death animation
+
+        yield return new WaitForSeconds(1f);
+
+        RestartLevel();
     }
 }
