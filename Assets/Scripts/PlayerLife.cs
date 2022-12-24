30cc43c5e47d7a056696391150c5b9a872d9f76a
Tyler Clardy <tclards@gmail.com>
2022-12-24 15:47:37 -0600

    Bugfix for death animation. Player sprite would reappear at end of animation and hold in place until respawn. Now fixed.

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,24 +11,29 @@ public class PlayerLife : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private PlayerStats ps;
 
+    [Header("Knockback Stats")]
+    [SerializeField] private float knockbackPower;
+
     private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Trap"))
+    {if (collision.gameObject.CompareTag("Trap"))
         {
             TakeDamage();
         }
+
     }
 
     // Helper Functions:  
     private void RestartLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1;
     }
 
     private void TakeDamage()
     {
         anim.SetTrigger("TakeDamage");
         ps.TakeDamage(1);
+        
 
         if (ps.Health == 0)
         {
@@ -43,8 +49,30 @@ public class PlayerLife : MonoBehaviour
 
         anim.SetTrigger("Death");               // play death animation
 
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.5f);
 
-        RestartLevel();
+        //RestartLevel();
     }
+
+
+    
+
+
+    // bouncepad?
+    //IEnumerator Knockback(float knockbackDuration, float knockbackPower, Vector3 knockbackDirection)
+    //{
+    //    float fTimer = 0f;
+
+    //    while (knockbackDuration > fTimer)
+    //    {
+    //        fTimer += Time.deltaTime;
+
+    //        rb.velocity = new Vector2(0, 0);
+    //        rb.AddForce(new Vector3(-knockbackDirection.x, -knockbackDirection.y + knockbackPower, transform.position.z));
+
+    //    }
+
+    //    yield return 0;
+    //}
+
 }
