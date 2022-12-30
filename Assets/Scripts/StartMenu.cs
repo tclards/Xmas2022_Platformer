4d584a2d87bb66460c10f72025f9045dba44ed1f
Tyler Clardy <tclards@gmail.com>
2022-12-30 12:12:23 -0600

    Added SFX for button clicks in menus

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -5,23 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
+    [SerializeField] public AudioSource clickSound;
+
     public void StartGame()
     {
+        clickSound.Play();
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void ExitGame()
     {
+        clickSound.Play();
+
         Application.Quit();
     }
 
     public void OptionsMenu()
     {
+        clickSound.Play();
+
 
     }
 
     public void OpenCredits()
     {
+        clickSound.Play();
+
 
     }
 
