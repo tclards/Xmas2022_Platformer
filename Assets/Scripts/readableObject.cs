7cd324da09f25ce3d82ac6f8eea09823f6af757c
Tyler Clardy <tclards@gmail.com>
2023-01-02 14:26:57 -0600

    Bugfix for signposts

--- Assets/Scripts/readableObject.cs
+++ Assets/Scripts/readableObject.cs
@@ -9,11 +9,14 @@ public class readableObject : MonoBehaviour
     [SerializeField] GameObject Prompt;
     [SerializeField] GameObject TextBoxUI;
 
+    private bool canActivate;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             Prompt.SetActive(true);
+            canActivate = true;
         }
     }
 
@@ -22,12 +25,14 @@ public class readableObject : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             Prompt.SetActive(false);
+            canActivate = false;
+            TextBoxUI.SetActive(false);
         }
     }
 
     private void Update()
     {
-        if (Input.GetButtonDown("Activate"))
+        if (Input.GetButtonDown("Activate") && canActivate)
         {
             if (TextBoxUI.activeInHierarchy == true)
             {
